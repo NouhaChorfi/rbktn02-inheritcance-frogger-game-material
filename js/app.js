783de2b2b69a7f825aca556d726a8d615a3683a9
unknown <nouhachorfi2@gmail.com>
2020-03-24 21:12:43 +0100

    collision works

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var Enemy = function(x,y) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x || 0;
-    this.y = y || Math.floor(Math.random()* 606);
+    this.y = y //|| Math.floor(Math.random()* 606);
     this.speed = 150
 };
 
@@ -22,15 +22,14 @@ Enemy.prototype.update = function(dt) {
     if (this.x > 505 ) { 
         this.x = -50
     }
+    
   
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    if(this.y === player.y && this.x ===player.x){
-        alert(this.y , player.y , this.x , player.y)
-    }
+   
 };
 
 // Now write your own player class
@@ -39,38 +38,34 @@ Enemy.prototype.render = function() {
 var Player = function(x,y) {
   this.sprite = 'images/char-boy.png'   
   this.x = x || 0;
-  this.y = y || 250;
+  this.y = y || 0;
   this.speed = 50;
 } 
 
 Player.prototype.update = function(){
+    for (var i=0 ; i< allEnemies.length ; i++){
+        if( 0 < this.y && this.y< 240 && allEnemies[i].y===this.y+30 && Math.floor(allEnemies[i].x) >= this.x-10 && Math.floor(allEnemies[i].x) <= this.x+10){ 
+            console.log(this.x, this.y ,Math.floor(allEnemies[i].x ))
+             this.x=0
+             this.y=360
+        }
+    }
+   
 };
 Player.prototype.render = function(){
  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
-// Player.prototype.handleInput = function(allowedKeys[e.keyCode]){  
-//     var key = e.keyCode ;
-// if (key === 38 && this.x < ctx.height ) { 
-//     this.x -= 8 ;
-// }if (key === 40 && this.x < ctx.height) { 
-//     this.x += 5 ;
-// } if (key === 37 && this.y < ctx.width) { 
-//     this.y -= 10 ;
-// } if (key === 39 && this.y < ctx.width) { 
-//     this.y += 10 ;
-// }
-
-// };
 
 Player.prototype.handleInput = function(key){
+  
     //  for (var key in obj){
           if (key=== "left" ) { 
               if(this.x < 0){
                   this.x=500
               }
               else{
-                this.x -= 50 ;
+                this.x -= 101 ;
               }
             }
             if (key=== "right" ) { 
@@ -78,14 +73,14 @@ Player.prototype.handleInput = function(key){
                     this.x=0
                 }
                 else{
-                  this.x += 50 ;
+                  this.x += 101 ;
                 }
             }
 
             if (key === 'up' && this.y > -5) { 
-                this.y -= 50 ;
+                this.y -= 80 ;
             } if (key === 'down' && this.y < 420) { 
-                this.y += 50 ;
+                this.y += 80 ;
             }
     //  }
       
@@ -94,11 +89,11 @@ Player.prototype.handleInput = function(key){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new Player(100,300);
+var player = new Player(0,360);
 var allEnemies = [];
-allEnemies.push(new Enemy(100,250));
+allEnemies.push(new Enemy(100,230));
 allEnemies.push(new Enemy(50,150));
-allEnemies.push(new Enemy(100,50));
+allEnemies.push(new Enemy(100,70));
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -111,3 +106,16 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+var collisition = function() { 
+    console.log("ok")
+
+    allEnemies[0].imageLeftSide = allEnemies[0].x; 
+    allEnemies[0].imageRightSide = allEnemies[0].x + allEnemies[0].imageWidth;  
+    allEnemies[0].imageTopSide = allEnemies[0].y;  
+    v.imageDownSide = allEnemies[0].y + allEnemies[0].imageHeight; 
+
+    player.imageLeftSide = player.x; 
+    player.imageRightSide = player.x + player.imageWidth;  
+    player.imageTopSide = player.y;  
+    player.imageDownSide = player.y + player.imageHeight; 
+}
